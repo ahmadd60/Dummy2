d5c5d634a0f8f1987152081e58ce68639b591bc4
Duaa Ahmad <ahmadd60@uwosh.edu>
2022-10-20 19:44:15 -0500

    First commit, created a new project and first test

---
+++ Lab4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2Solution
+{ 
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            IBusinessLogic ibl = new BusinessLogic();
+        }
+    }
+}
